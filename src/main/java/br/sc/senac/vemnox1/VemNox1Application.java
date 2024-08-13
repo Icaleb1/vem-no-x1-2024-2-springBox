f9b468dc9cfec3419c44b29190d96387f5fb7f4c
caleb.oliveira <caleb.oliveira@FLE104D02.mshome.net>
2024-08-13 10:51:31 -0300

    primeiro commit

---
+++ src/main/java/br/sc/senac/vemnox1/VemNox1Application.java
@@ -0,0 +1,13 @@
+package br.sc.senac.vemnox1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemNox1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemNox1Application.class, args);
+	}
+
+}
